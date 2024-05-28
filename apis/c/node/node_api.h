6d28c4b994c0a34d2bb4c8d716e7097f7422b753
Raghava Uppuluri <raghava.upp13@gmail.com>
2024-05-28 01:37:06 -0700

    added float array comms types

--- apis/c/node/node_api.h
+++ apis/c/node/node_api.h
@@ -6,8 +6,7 @@ void free_dora_context(void *dora_context);
 void *dora_next_event(void *dora_context);
 void free_dora_event(void *dora_event);
 
-enum DoraEventType
-{
+enum DoraEventType {
     DoraEventType_Stop,
     DoraEventType_Input,
     DoraEventType_InputClosed,
@@ -18,5 +17,10 @@ enum DoraEventType read_dora_event_type(void *dora_event);
 
 void read_dora_input_id(void *dora_event, char **out_ptr, size_t *out_len);
 void read_dora_input_data(void *dora_event, char **out_ptr, size_t *out_len);
+void read_dora_input_data_f32(void *dora_event, float **out_ptr,
+                              size_t *out_len);
 
-int dora_send_output(void *dora_context, char *id_ptr, size_t id_len, char *data_ptr, size_t data_len);
+int dora_send_output(void *dora_context, char *id_ptr, size_t id_len,
+                     char *data_ptr, size_t data_len);
+int dora_send_output_f32(void *dora_context, char *id_ptr, size_t id_len,
+                         float *data_ptr, size_t data_len);
--- apis/c/node/src/lib.rs
+++ apis/c/node/src/lib.rs
@@ -1,10 +1,10 @@
 #![deny(unsafe_op_in_unsafe_fn)]
-
 use arrow_array::UInt8Array;
+use arrow_array::{Array, Float32Array, Int32Array};
 use dora_node_api::{arrow::array::AsArray, DoraNode, Event, EventStream};
 use eyre::Context;
+use std::sync::Arc;
 use std::{ffi::c_void, ptr, slice};
-
 pub const HEADER_NODE_API: &str = include_str!("../node_api.h");
 
 struct DoraContext {
@@ -195,6 +195,58 @@ pub unsafe extern "C" fn read_dora_input_data(
     }
 }
 
+/// Reads out the data of the given input event.
+///
+/// Writes the `out_ptr` and `out_len` with the start pointer and length of the
+/// input's data array. The data array is a raw byte array, whose format
+/// depends on the source operator/node.
+///
+/// Writes a null pointer and length `0` if the given event is not an input event
+/// or when an input event has no associated data.
+///
+/// ## Safety
+///
+/// The `event` argument must be a dora event received through
+/// [`dora_next_event`]. The event must be still valid, i.e., not
+/// freed yet. The returned `out_ptr` must not be used after
+/// freeing the `event`, since it points directly into the event's
+/// memory.
+#[no_mangle]
+pub unsafe extern "C" fn read_dora_input_data_f32(
+    event: *const (),
+    out_ptr: *mut *const f32,
+    out_len: *mut usize,
+) {
+    let event: &Event = unsafe { &*event.cast() };
+
+    match event {
+        Event::Input { data, metadata, .. } => match metadata.type_info.data_type {
+            dora_node_api::arrow::datatypes::DataType::Float32 => {
+                let array: &Float32Array = data.as_primitive();
+                let ptr = array.values().as_ptr();
+                unsafe {
+                    *out_ptr = ptr;
+                    *out_len = metadata.type_info.len;
+                }
+            }
+            dora_node_api::arrow::datatypes::DataType::Null => unsafe {
+                *out_ptr = ptr::null();
+                *out_len = 0;
+            },
+            _ => {
+                panic!(
+                    "You used {}, must use Float32!",
+                    metadata.type_info.data_type
+                );
+            }
+        },
+        _ => unsafe {
+            *out_ptr = ptr::null();
+            *out_len = 0;
+        },
+    }
+}
+
 /// Frees the given dora event.
 ///
 /// ## Safety
@@ -240,21 +292,80 @@ pub unsafe extern "C" fn dora_send_output(
         }
     }
 }
-
-unsafe fn try_send_output(
+/// Sends the given output to subscribed dora nodes/operators.
+///
+/// The `id_ptr` and `id_len` fields must be the start pointer and length of an
+/// UTF8-encoded string. The ID string must correspond to one of the node's
+/// outputs specified in the dataflow YAML file.
+///
+/// The `data_ptr` and `data_len` fields must be the start pointer and length
+/// a byte array. The dora API sends this data as-is, without any processing.
+///
+/// ## Safety
+///
+/// - The `id_ptr` and `id_len` fields must be the start pointer and length of an
+///   UTF8-encoded string.
+/// - The `data_ptr` and `data_len` fields must be the start pointer and length
+///   a byte array.
+#[no_mangle]
+pub unsafe extern "C" fn dora_send_output_f32(
     context: *mut c_void,
     id_ptr: *const u8,
     id_len: usize,
-    data_ptr: *const u8,
+    data_ptr: *const f32,
     data_len: usize,
-) -> eyre::Result<()> {
+) -> isize {
+    match unsafe { try_send_output(context, id_ptr, id_len, data_ptr, data_len) } {
+        Ok(()) => 0,
+        Err(err) => {
+            tracing::error!("{err:?}");
+            -1
+        }
+    }
+}
+
+pub trait ToArrow {
+    fn to_arrow(self) -> Arc<dyn Array>;
+}
+
+impl ToArrow for &[f32] {
+    fn to_arrow(self) -> Arc<dyn Array> {
+        let array = Float32Array::from(self.to_vec());
+        Arc::new(array)
+    }
+}
+
+impl ToArrow for &[i32] {
+    fn to_arrow(self) -> Arc<dyn Array> {
+        let array = Int32Array::from(self.to_vec());
+        Arc::new(array)
+    }
+}
+
+impl ToArrow for &[u8] {
+    fn to_arrow(self) -> Arc<dyn Array> {
+        let array = UInt8Array::from(self.to_vec());
+        Arc::new(array)
+    }
+}
+
+unsafe fn try_send_output<T>(
+    context: *mut c_void,
+    id_ptr: *const u8,
+    id_len: usize,
+    data_ptr: *const T,
+    data_len: usize,
+) -> eyre::Result<()>
+where
+    for<'a> &'a [T]: ToArrow,
+{
     let context: &mut DoraContext = unsafe { &mut *context.cast() };
     let id = std::str::from_utf8(unsafe { slice::from_raw_parts(id_ptr, id_len) })?;
     let output_id = id.to_owned().into();
+
     let data = unsafe { slice::from_raw_parts(data_ptr, data_len) };
+    let data_array = data.to_arrow();
     context
         .node
-        .send_output_raw(output_id, Default::default(), data.len(), |out| {
-            out.copy_from_slice(data);
-        })
+        .send_output(output_id, Default::default(), data_array)
 }
