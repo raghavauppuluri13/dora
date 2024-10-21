15babb8e6a0972c4f85fa4b802326e06436e1365
Raghava Uppuluri <raghava.upp13@gmail.com>
2024-10-20 23:01:24 -0500

    added i32 and adjusted naming

--- apis/c/node/node_api.h
+++ apis/c/node/node_api.h
@@ -19,12 +19,18 @@ enum DoraEventType {
 enum DoraEventType read_dora_event_type(void *dora_event);
 
 void read_dora_input_id(void *dora_event, char **out_ptr, size_t *out_len);
-void read_dora_input_data(void *dora_event, char **out_ptr, size_t *out_len);
+void read_dora_input_data_u8(void *dora_event, uint8_t **out_ptr,
+                             size_t *out_len);
+void read_dora_input_data_i32(void *dora_event, int **out_ptr, size_t *out_len);
 void read_dora_input_data_f32(void *dora_event, float **out_ptr,
                               size_t *out_len);
+void read_dora_input_data_u64(void *dora_event, uint64_t **out_ptr,
+                              size_t *out_len);
 
-int dora_send_output(void *dora_context, char *id_ptr, size_t id_len,
-                     char *data_ptr, size_t data_len);
+int dora_send_output_u8(void *dora_context, char *id_ptr, size_t id_len,
+                        uint8_t *data_ptr, size_t data_len);
+int dora_send_output_i32(void *dora_context, char *id_ptr, size_t id_len,
+                         int *data_ptr, size_t data_len);
 int dora_send_output_f32(void *dora_context, char *id_ptr, size_t id_len,
                          float *data_ptr, size_t data_len);
 int dora_send_output_u64(void *dora_context, char *id_ptr, size_t id_len,
--- apis/c/node/src/lib.rs
+++ apis/c/node/src/lib.rs
@@ -146,24 +146,8 @@ pub unsafe extern "C" fn read_dora_input_id(
     }
 }
 
-/// Reads out the data of the given input event.
-///
-/// Writes the `out_ptr` and `out_len` with the start pointer and length of the
-/// input's data array. The data array is a raw byte array, whose format
-/// depends on the source operator/node.
-///
-/// Writes a null pointer and length `0` if the given event is not an input event
-/// or when an input event has no associated data.
-///
-/// ## Safety
-///
-/// The `event` argument must be a dora event received through
-/// [`dora_next_event`]. The event must be still valid, i.e., not
-/// freed yet. The returned `out_ptr` must not be used after
-/// freeing the `event`, since it points directly into the event's
-/// memory.
 #[no_mangle]
-pub unsafe extern "C" fn read_dora_input_data(
+pub unsafe extern "C" fn read_dora_input_data_u8(
     event: *const (),
     out_ptr: *mut *const u8,
     out_len: *mut usize,
@@ -184,8 +168,7 @@ pub unsafe extern "C" fn read_dora_input_data(
                 *out_len = 0;
             },
             _ => {
-                todo!("dora C++ Node does not yet support higher level type of arrow. Only UInt8. 
-                The ultimate solution should be based on arrow FFI interface. Feel free to contribute :)")
+                panic!("You used {}, must use U8!", metadata.type_info.data_type);
             }
         },
         _ => unsafe {
@@ -195,22 +178,38 @@ pub unsafe extern "C" fn read_dora_input_data(
     }
 }
 
-/// Reads out the data of the given input event.
-///
-/// Writes the `out_ptr` and `out_len` with the start pointer and length of the
-/// input's data array. The data array is a raw byte array, whose format
-/// depends on the source operator/node.
-///
-/// Writes a null pointer and length `0` if the given event is not an input event
-/// or when an input event has no associated data.
-///
-/// ## Safety
-///
-/// The `event` argument must be a dora event received through
-/// [`dora_next_event`]. The event must be still valid, i.e., not
-/// freed yet. The returned `out_ptr` must not be used after
-/// freeing the `event`, since it points directly into the event's
-/// memory.
+#[no_mangle]
+pub unsafe extern "C" fn read_dora_input_data_i32(
+    event: *const (),
+    out_ptr: *mut *const i32,
+    out_len: *mut usize,
+) {
+    let event: &Event = unsafe { &*event.cast() };
+    match event {
+        Event::Input { data, metadata, .. } => match metadata.type_info.data_type {
+            dora_node_api::arrow::datatypes::DataType::Int32 => {
+                let array: &Int32Array = data.as_primitive();
+                let ptr = array.values().as_ptr();
+                unsafe {
+                    *out_ptr = ptr;
+                    *out_len = metadata.type_info.len;
+                }
+            }
+            dora_node_api::arrow::datatypes::DataType::Null => unsafe {
+                *out_ptr = ptr::null();
+                *out_len = 0;
+            },
+            _ => {
+                panic!("You used {}, must use Int32!", metadata.type_info.data_type);
+            }
+        },
+        _ => unsafe {
+            *out_ptr = ptr::null();
+            *out_len = 0;
+        },
+    }
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn read_dora_input_data_f32(
     event: *const (),
@@ -247,6 +246,42 @@ pub unsafe extern "C" fn read_dora_input_data_f32(
     }
 }
 
+#[no_mangle]
+pub unsafe extern "C" fn read_dora_input_data_u64(
+    event: *const (),
+    out_ptr: *mut *const u64,
+    out_len: *mut usize,
+) {
+    let event: &Event = unsafe { &*event.cast() };
+
+    match event {
+        Event::Input { data, metadata, .. } => match metadata.type_info.data_type {
+            dora_node_api::arrow::datatypes::DataType::UInt64 => {
+                let array: &UInt64Array = data.as_primitive();
+                let ptr = array.values().as_ptr();
+                unsafe {
+                    *out_ptr = ptr;
+                    *out_len = metadata.type_info.len;
+                }
+            }
+            dora_node_api::arrow::datatypes::DataType::Null => unsafe {
+                *out_ptr = ptr::null();
+                *out_len = 0;
+            },
+            _ => {
+                panic!(
+                    "You used {}, must use UInt64!",
+                    metadata.type_info.data_type
+                );
+            }
+        },
+        _ => unsafe {
+            *out_ptr = ptr::null();
+            *out_len = 0;
+        },
+    }
+}
+
 /// Frees the given dora event.
 ///
 /// ## Safety
@@ -261,23 +296,8 @@ pub unsafe extern "C" fn free_dora_event(event: *mut c_void) {
     let _: Box<Event> = unsafe { Box::from_raw(event.cast()) };
 }
 
-/// Sends the given output to subscribed dora nodes/operators.
-///
-/// The `id_ptr` and `id_len` fields must be the start pointer and length of an
-/// UTF8-encoded string. The ID string must correspond to one of the node's
-/// outputs specified in the dataflow YAML file.
-///
-/// The `data_ptr` and `data_len` fields must be the start pointer and length
-/// a byte array. The dora API sends this data as-is, without any processing.
-///
-/// ## Safety
-///
-/// - The `id_ptr` and `id_len` fields must be the start pointer and length of an
-///   UTF8-encoded string.
-/// - The `data_ptr` and `data_len` fields must be the start pointer and length
-///   a byte array.
 #[no_mangle]
-pub unsafe extern "C" fn dora_send_output(
+pub unsafe extern "C" fn dora_send_output_u8(
     context: *mut c_void,
     id_ptr: *const u8,
     id_len: usize,
@@ -292,21 +312,24 @@ pub unsafe extern "C" fn dora_send_output(
         }
     }
 }
-/// Sends the given output to subscribed dora nodes/operators.
-///
-/// The `id_ptr` and `id_len` fields must be the start pointer and length of an
-/// UTF8-encoded string. The ID string must correspond to one of the node's
-/// outputs specified in the dataflow YAML file.
-///
-/// The `data_ptr` and `data_len` fields must be the start pointer and length
-/// a byte array. The dora API sends this data as-is, without any processing.
-///
-/// ## Safety
-///
-/// - The `id_ptr` and `id_len` fields must be the start pointer and length of an
-///   UTF8-encoded string.
-/// - The `data_ptr` and `data_len` fields must be the start pointer and length
-///   a byte array.
+
+#[no_mangle]
+pub unsafe extern "C" fn dora_send_output_i32(
+    context: *mut c_void,
+    id_ptr: *const u8,
+    id_len: usize,
+    data_ptr: *const i32,
+    data_len: usize,
+) -> isize {
+    match unsafe { try_send_output(context, id_ptr, id_len, data_ptr, data_len) } {
+        Ok(()) => 0,
+        Err(err) => {
+            tracing::error!("{err:?}");
+            -1
+        }
+    }
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn dora_send_output_f32(
     context: *mut c_void,
@@ -323,22 +346,6 @@ pub unsafe extern "C" fn dora_send_output_f32(
         }
     }
 }
-
-/// Sends the given output to subscribed dora nodes/operators.
-///
-/// The `id_ptr` and `id_len` fields must be the start pointer and length of an
-/// UTF8-encoded string. The ID string must correspond to one of the node's
-/// outputs specified in the dataflow YAML file.
-///
-/// The `data_ptr` and `data_len` fields must be the start pointer and length
-/// a byte array. The dora API sends this data as-is, without any processing.
-///
-/// ## Safety
-///
-/// - The `id_ptr` and `id_len` fields must be the start pointer and length of an
-///   UTF8-encoded string.
-/// - The `data_ptr` and `data_len` fields must be the start pointer and length
-///   a byte array.
 #[no_mangle]
 pub unsafe extern "C" fn dora_send_output_u64(
     context: *mut c_void,
